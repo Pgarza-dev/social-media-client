48d2ceca063179eee29a16987b48d3674a167be7
Pablo Gaza <p.garza1983@gmail.com>
2023-11-07 22:46:28 +0100

    adds cypress login testing

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,21 @@
-describe("url", () => {
-    it("what the test does must be accurate", () => {
-      cy.visit("/");
-      cy.get("#loginEmail").type("enter email", { delay: 500 });
-      cy.get("#loginPassword").type("enter password", { delay: 500 });
-      cy.get('h1').contains("Noroff")
-    })
-  })
+describe("Login Authentication", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.clearLocalStorage();
+  });
+
+  it("will log in", () => {
+    cy.visit("/");
+    cy.wait(1000);
+    cy.get(".btn-close:visible").click();
+    cy.wait(500);
+    cy.get("button[data-auth='login']:visible").click();
+    cy.wait(2000);
+    cy.get("input[type='email']:visible").type("fake@stud.noroff.no", {
+      delay: 500,
+    });
+    cy.get("input[type='password']:visible").type("fakefake", { delay: 500 });
+    cy.get(".btn-success:visible").click();
+    cy.wait(500);
+  });
+});
