7f60bb8168261b19a0bd1b6a84f1776a447978c7
Pablo Gaza <p.garza1983@gmail.com>
2023-11-08 10:24:28 +0100

    updates cy.login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,15 +7,13 @@ describe("Login Authentication", () => {
   it("will log in", () => {
     cy.visit("/");
     cy.wait(1000);
-    cy.get(".btn-close:visible").click();
-    cy.wait(500);
-    cy.get("button[data-auth='login']:visible").click();
+    cy.get("button[data-auth='login']:visible").should("exist").click();
     cy.wait(2000);
     cy.get("input[type='email']:visible").type("fake@stud.noroff.no", {
       delay: 500,
     });
     cy.get("input[type='password']:visible").type("fakefake", { delay: 500 });
-    cy.get(".btn-success:visible").click();
+    cy.get(".btn-success:visible").should("exist").click();
     cy.wait(500);
   });
 });
