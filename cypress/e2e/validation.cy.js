4fb7a9ee4f0de75cf0ed6fadbe83ecb68f2a1941
Pablo Gaza <p.garza1983@gmail.com>
2023-11-08 13:37:46 +0100

    adds validation.cy.js testing

---
+++ cypress/e2e/validation.cy.js
@@ -0,0 +1,56 @@
+const invalidEmail = "invalid@stud.noroff.no";
+const invalidPassword = "invalidPassword";
+
+describe("Login not authenticated", () => {
+  it("will deny access with invalid credentials", () => {
+    cy.visit("/");
+    cy.wait(1000);
+    cy.get("button[data-auth='login']:visible").should("exist").click();
+    cy.wait(2000);
+    cy.get("input[type='email']:visible").type(invalidEmail, {
+      delay: 500,
+    });
+    cy.get("input[type='password']:visible").type(invalidPassword, {
+      delay: 500,
+    });
+    cy.get(".btn-success:visible").should("exist").click();
+    cy.wait(1000);
+  });
+
+  it("will deny access with empty credentials", () => {
+    cy.visit("/");
+    cy.wait(1000);
+    cy.get("button[data-auth='login']:visible").should("exist").click();
+    cy.wait(2000);
+    cy.get("input[type='email']:visible", { delay: 500 });
+    cy.get("input[type='password']:visible", { delay: 500 });
+    cy.get(".btn-success:visible").should("exist").click();
+    cy.wait(1000);
+  });
+
+  it("will deny access with empty email", () => {
+    cy.visit("/");
+    cy.wait(1000);
+    cy.get("button[data-auth='login']:visible").should("exist").click();
+    cy.wait(2000);
+    cy.get("input[type='email']:visible", { delay: 500 });
+    cy.get("input[type='password']:visible").type(invalidPassword, {
+      delay: 500,
+    });
+    cy.get(".btn-success:visible").should("exist").click();
+    cy.wait(1000);
+  });
+
+  it("will deny access with empty password", () => {
+    cy.visit("/");
+    cy.wait(1000);
+    cy.get("button[data-auth='login']:visible").should("exist").click();
+    cy.wait(2000);
+    cy.get("input[type='email']:visible").type(invalidEmail, {
+      delay: 500,
+    });
+    cy.get("input[type='password']:visible", { delay: 500 });
+    cy.get(".btn-success:visible").should("exist").click();
+    cy.wait(1000);
+  });
+});
